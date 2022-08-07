c363c8c63376a0130e37e43d8b2d81167964ef2c
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-08-07 18:34:33 -0500

    update

--- src/compo/Profile.js
+++ src/compo/Profile.js
@@ -4,7 +4,7 @@ const Profile = () => {
   return (
     <div className="profile-container">
      <div className="profile-image-container">
-     <img className="profile-image" src ='https://images.unsplash.com/photo-1659892438649-c9e1b8233592?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' alt=''/>
+     <img className="profile-image" src ='https://finalappchinchorro0029dd132ee54bd4b414de81bee3b82034-dev.s3.amazonaws.com/public/foto-perfil.jpeg' alt=''/>
      </div>
     </div>
   )
